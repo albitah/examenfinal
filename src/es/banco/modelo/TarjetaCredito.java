25ec69501d00971199286d20996ae06c2c7031e6
1 <1@06-PC>
2014-11-26 13:37:25 +0100

    se crean los constructores y los getters y setters

--- src/es/banco/modelo/TarjetaCredito.java
+++ src/es/banco/modelo/TarjetaCredito.java
@@ -9,7 +9,41 @@ public class TarjetaCredito {
 	private int numeroComprobacion;
 	private int contraseña;
 	
+	
 	//contructores
+	public TarjetaCredito(int id, int numero, int cupoMaximo,
+			int cupoDisponible, boolean tipoTarjeta, int numeroComprobacion,
+			int contraseña) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.cupoMaximo = cupoMaximo;
+		this.cupoDisponible = cupoDisponible;
+		this.tipoTarjeta = tipoTarjeta;
+		this.numeroComprobacion = numeroComprobacion;
+		this.contraseña = contraseña;
+	}
+
+
+	public TarjetaCredito(int numero, int cupoMaximo, int cupoDisponible,
+			boolean tipoTarjeta, int numeroComprobacion, int contraseña) {
+		super();
+		this.numero = numero;
+		this.cupoMaximo = cupoMaximo;
+		this.cupoDisponible = cupoDisponible;
+		this.tipoTarjeta = tipoTarjeta;
+		this.numeroComprobacion = numeroComprobacion;
+		this.contraseña = contraseña;
+	}
+
+
+	public TarjetaCredito() {
+		super();
+	}
+	
+	
+	//getters y setters
+	
 	
 
 }
