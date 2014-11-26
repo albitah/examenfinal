ccf40c152a4ca610c0d9637fdaa9ed07a27528a2
1 <1@06-PC>
2014-11-26 13:34:38 +0100

    se crea la clase tarjeta de credito

---
+++ src/es/banco/modelo/TarjetaCredito.java
@@ -0,0 +1,15 @@
+package es.banco.modelo;
+
+public class TarjetaCredito {
+	private int id;
+	private int numero;
+	private int cupoMaximo;
+	private int cupoDisponible;
+	private boolean tipoTarjeta;
+	private int numeroComprobacion;
+	private int contraseña;
+	
+	//contructores
+	
+
+}
